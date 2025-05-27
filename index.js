8a3e87c965e9c13fa239630e23ca8cd92c78479e
Ivanna <kotepodlsd9@gmail.com>
2025-05-27 16:22:04 +0200

    all

---
+++ index.js
@@ -0,0 +1,39 @@
+function sumArray(numbers) {
+    return numbers.reduce((sum, num) => sum + num, 0);
+}
+function createUser(name, age, isActive = true) {
+    return {
+        name,
+        age,
+        isActive
+    };
+}
+var OrderStatus;
+(function (OrderStatus) {
+    OrderStatus["Pending"] = "Pending";
+    OrderStatus["Shipped"] = "Shipped";
+    OrderStatus["Delivered"] = "Delivered";
+    OrderStatus["Cancelled"] = "Cancelled";
+})(OrderStatus || (OrderStatus = {}));
+function getOrderStatus(status) {
+    switch (status) {
+        case OrderStatus.Pending:
+            return 'Замовлення очікує на обробку';
+        case OrderStatus.Shipped:
+            return 'Замовлення було відправлено';
+        case OrderStatus.Delivered:
+            return 'Замовлення доставлено';
+        case OrderStatus.Cancelled:
+            return 'Замовлення скасовано';
+        default:
+            throw new Error('Невідомий статус замовлення');
+    }
+}
+console.log(sumArray([1, 2, 3, 4]));
+console.log(sumArray([]));
+const newUser = createUser('Анна', 25);
+console.log(newUser);
+console.log(getOrderStatus(OrderStatus.Pending));
+console.log(getOrderStatus(OrderStatus.Shipped));
+console.log(getOrderStatus(OrderStatus.Delivered));
+console.log(getOrderStatus(OrderStatus.Cancelled));
