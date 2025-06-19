1a1db665e8db888dddec45bf7c1ca04899acc911
Ivanna <kotepodlsd9@gmail.com>
2025-06-19 20:32:49 +0200

    all

--- src/hooks/useTheme.js
+++ src/hooks/useTheme.js
@@ -1,23 +1,26 @@
-import React, {createContext, useState, useEffect} from "react";
+import React, { createContext, useState, useEffect } from "react";
+import { THEMES } from "../constants/theme"; 
 
 export const ThemeContext = createContext();
 
-export const ThemeProvider = ({children}) => {
-    const storedTheme = localStorage.getItem("theme") || "light";
-    const [theme, setTheme] = useState(storedTheme);
+export const ThemeProvider = ({ children }) => {
+  const storedTheme = localStorage.getItem("theme") || THEMES.LIGHT;
+  const [theme, setTheme] = useState(storedTheme);
 
-    useEffect(() => {
-        document.body.className = theme;
-        localStorage.setItem("theme", theme);
-    }, [theme]);
+  useEffect(() => {
+    document.body.className = theme;
+    localStorage.setItem("theme", theme);
+  }, [theme]);
 
-    const toggleTheme = () => {
-       setTheme(prevTheme => (prevTheme === "light" ? "dark" : "light"));
-    };
-
-    return (
-        <ThemeContext.Provider value={{theme, toggleTheme}}>
-            {children}
-        </ThemeContext.Provider>
+  const toggleTheme = () => {
+    setTheme((prevTheme) =>
+      prevTheme === THEMES.LIGHT ? THEMES.DARK : THEMES.LIGHT
     );
-}
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
